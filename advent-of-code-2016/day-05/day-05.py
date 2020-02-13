82c9caed125df4f3e6ec6e00d408d507c5bcd82f
unknown <jareddrayton@users.noreply.github.com>
2020-02-13 21:35:32 +0000

    Day-05 2016

---
+++ advent-of-code-2016/day-05/day-05.py
@@ -0,0 +1,52 @@
+import hashlib
+
+puzzle_input = "wtnhxymk"
+
+def number(prefix="00000"):
+
+    index = 0
+
+    password = ""
+
+    while len(password) < 8:
+
+        b = puzzle_input + str(index)
+        test = hashlib.md5(b.encode())
+        test = test.hexdigest()
+
+        if test[:len(prefix)] == prefix:
+            
+            password += test[5]
+
+        if len(password) == 8:
+            return password
+
+        index += 1
+
+print("Part One:", number())
+
+def numberb(prefix="00000"):
+
+    index = 0
+
+    password = [None, None, None, None, None, None, None, None]
+
+    passa = 0
+
+    while passa < 8:
+
+        b = puzzle_input + str(index)
+        test = hashlib.md5(b.encode())
+        test = test.hexdigest()
+
+        if test[:len(prefix)] == prefix:
+            if test[5].isdigit() and int(test[5]) < 8 and password[int(test[5])] == None:
+                password[int(test[5])] = test[6]
+                passa += 1
+
+        if passa == 8:
+            return "".join(password)
+
+        index += 1
+
+print("Part Two:", numberb())
