7a5c714fe5d86e82a4374d04cd430c963ba805d6
unknown <jareddrayton@users.noreply.github.com>
2020-02-13 08:31:11 +0000

    Day-04 2016

---
+++ advent-of-code-2016/day-04/day-04a.py
@@ -0,0 +1,41 @@
+with open("day-04-input.txt", 'r') as f:
+    room_scrambled = f.readlines()
+
+room_parsed = []
+
+for room in room_scrambled:
+    
+    room, hash_part = room.strip().split('[')
+    hash_part = hash_part.strip(']')
+    sector_id = int("".join([char for char in room if char.isdigit()]))
+    encryp_name = "".join([char for char in room if char.isalpha()])
+    room_parsed.append([encryp_name, hash_part, sector_id])
+
+def isreal(l):
+
+    template = "abcdefghijklmnopqrstuvwxyz"
+
+    freq = []
+
+    for c in template:
+        freq.append([c, l[0].count(c)])
+
+    check = []
+
+    for _ in range(0,5):
+        check.append(max(freq, key = lambda x: x[1]))
+        freq.remove(max(freq, key = lambda x: x[1]))
+
+    check = "".join([y for x in check for y in x][::2])
+
+    if check == l[1]:
+        return l[2]
+    else:
+        return 0
+
+total = 0
+
+for room in room_parsed:
+    total += isreal(room)
+
+print("Part One:", total)
---
+++ advent-of-code-2016/day-04/day-04b.py
@@ -0,0 +1,37 @@
+with open("day-04-input.txt", 'r') as f:
+    room_scrambled = f.readlines()
+
+template = "abcdefghijklmnopqrstuvwxyz"
+
+room_parsed = []
+
+for room in room_scrambled:
+    room, hash_part = room.strip().split('[')
+    hash_part = hash_part.strip(']')
+    sector_id = int("".join([char for char in room if char.isdigit()]))
+    encryp_name = "".join([char for char in room if char.isdigit() == False])
+    room_parsed.append([encryp_name, hash_part, sector_id])
+
+def cipher(cipherd_text, offset):
+    
+    alphabet_string = 'abcdefghijklmnopqrstuvwxyz'
+
+    cipher = [x for x in range(len(alphabet_string))]
+
+    cipher = cipher[offset:] + cipher[:offset]
+
+    trans = ""
+
+    for letter in cipherd_text:
+        if letter.isalpha() == True:
+            trans += (alphabet_string[cipher[alphabet_string.rindex(letter)]])
+        else:
+            trans += " "
+    return trans
+
+for i in range(len(room_parsed)):
+    room_parsed[i][0] = cipher(room_parsed[i][0], room_parsed[i][2]%26)
+
+for room in room_parsed:
+    if room[0].count("pole") > 0:
+        print("Part Two:", room[2])
