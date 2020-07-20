93710c2297e3fc0ba55c98dbcc8557d761f64033
jareddrayton <jareddrayton@users.noreply.github.com>
2020-07-20 23:35:10 +0100

    More refactoring on day-10 2019

--- advent-of-code-2019/day-10/day-10-refactor.py
+++ advent-of-code-2019/day-10/day-10-refactor.py
@@ -10,50 +10,55 @@ def extract_asteroid_coords():
     return asteroid_coords
 
 
-def calculate_angle(ast_x_coord, sta_x_coord, sta_y_coord, ast_y_coord):
-    angle = math.atan2(ast_x_coord - sta_x_coord, sta_y_coord - ast_y_coord)
+def calculate_angle(x_diff, y_diff):
+    angle = math.atan2(x_diff, y_diff)
     angle = math.degrees(angle)
     if angle < 0:
         angle += 360
     return angle
 
 
-def build_station_info():
+def station_positions(asteroids, potential_station):
+    x = []
+    for asteroid in asteroids:
+        if asteroid != potential_station:
+            x_diff = asteroid[0] - potential_station[0]
+            y_diff = potential_station[1] - asteroid[1]
+            angle = calculate_angle(x_diff, y_diff)
+            euc_distance = math.hypot(x_diff, y_diff)
+            x.append((angle, euc_distance, asteroid[0], asteroid[1]))
+    return x
+
+
+def build_stations_dict():
     stations = {}
     asteroids = extract_asteroid_coords()
-    for potential_station in asteroids:
-        stations[potential_station] = []
-        for asteroid in asteroids:
-            if asteroid != potential_station:
-                sta_x_coord, sta_y_coord = potential_station
-                ast_x_coord, ast_y_coord = asteroid
-                angle = calculate_angle(ast_x_coord, sta_x_coord, sta_y_coord, ast_y_coord)
-                euc_distance = math.hypot(ast_x_coord - sta_x_coord, ast_y_coord - sta_y_coord)
-                stations[potential_station].append((angle, ast_x_coord, ast_y_coord, euc_distance))
+    for station in asteroids:
+        stations[station] = station_positions(asteroids, station)
     return stations
 
 
-def part_one(potential_stations):
-    unique_angles = [len(set([i[0] for i in v])) for v in potential_stations.values()]
-    return max(unique_angles)
+def part_one(stations):
+    test = [(len(set([i[0] for i in v])), k) for k, v in stations.items()]
+    return sorted(test)[-1]
 
 
 def part_two(stations):
-    test = [(len(set([i[0] for i in v])), k) for k, v in stations.items()]
-    monitoring_station = sorted(test)[-1][1]
-    
-    asteroids_ordered = sorted(stations[monitoring_station], key=itemgetter(0, 3))
+    monitoring_station = part_one(stations)[1]
+    asteroids_ordered = sorted(stations[monitoring_station], key=itemgetter(0, 1))
     asteroids_destroy_order = [asteroids_ordered[0]]
     
     for asteroid in cycle(asteroids_ordered):
         if asteroids_destroy_order[-1][0] != asteroid[0]:
             asteroids_destroy_order.append(asteroid)
         if len(asteroids_destroy_order) == 200:
+            nth_asteroid = asteroids_destroy_order[-1]
             break
-    return asteroids_destroy_order[-1][1] * 100 + asteroids_destroy_order[-1][2]
+
+    return nth_asteroid[2] * 100 + nth_asteroid[3]
 
 
 if __name__ == '__main__':
-    stations = build_station_info()
-    print("Part 1:", part_one(stations))
+    stations = build_stations_dict()
+    print("Part 1:", part_one(stations)[0])
     print("Part 2:", part_two(stations))
