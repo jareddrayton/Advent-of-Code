205524a2c4dad615d6a421e8c399334ee6ee18a8
jareddrayton <jareddrayton@users.noreply.github.com>
2021-11-30 20:29:23 +0000

    Modify to work on unix.

--- scaffold.py
+++ scaffold.py
@@ -1,6 +1,6 @@
 import os
 
-year = "2020"
+year = '2021'
 
-for i in range(1,26):
-    os.makedirs("advent_of_code_{}\\day_{:02d}".format(year, i))
+for i in range(1, 26):
+    os.makedirs(os.path.join(f'advent_of_code_{year}', 'day_{:02d}'.format(i)))
